13cf6aa65d8d70fae3267fbc5d032d48c9445433
blackkucai <77704864+blackkucai@users.noreply.github.com>
2021-01-23 02:14:48 +0700

    Delete getfile.py

--- getfile.py
+++
@@ -1,7 +0,0 @@
-# getfile.py
-# created by blackkucai
-import os
-                       
-                                                  
-def mixin(infile, infile1, outfile):                                                                          
-    os.system(f"ffmpeg -y -loglevel repeat+info -i {infile} -i {infile1} -c copy -map 0:v:0 -map 1:a:0 {outfile}")
